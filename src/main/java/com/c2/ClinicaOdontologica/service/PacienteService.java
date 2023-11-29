ab4d5265dd31e2e27e7f03bba2fb64e57c360781
MariaUgas <marria42@gmail.com>
2023-11-28 21:42:00 -0300

    Manejo de la fecha de ingreso desde el servicio

--- src/main/java/com/c2/ClinicaOdontologica/service/PacienteService.java
+++ src/main/java/com/c2/ClinicaOdontologica/service/PacienteService.java
@@ -5,6 +5,7 @@ import com.c2.ClinicaOdontologica.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +15,7 @@ public class PacienteService {
     private PacienteRepository pacienteRepository;
 
     public Paciente registrarPaciente(Paciente paciente){
+        paciente.setFechaIngreso(LocalDate.now());
         return  pacienteRepository.save(paciente);
     }
     public void actualizarPaciente(Paciente paciente){
